345902268db68d1edf301cd66623e313a6f9d533
AndrewGagliottiUnibo <andrew.gagliotti@studio.unibo.it>
2022-10-04 16:25:40 +0200

    interface

---
+++ 2022/03/36-design-from-scratch/src/main/java/it/unibo/shapes/api/Shapes.java
@@ -0,0 +1,15 @@
+package it.unibo.shapes.api;
+
+public interface Shapes {
+    
+    /**
+     * @return perimeter value.
+     */
+    double computePerimeter();
+
+    /**
+     * @return area value.
+     */
+    double computeArea();
+
+}
