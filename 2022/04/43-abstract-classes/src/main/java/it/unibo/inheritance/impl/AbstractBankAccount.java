718a4bf064094e06fe3ed90653cf68f4831350a2
AndrewGagliottiUnibo <andrew.gagliotti@studio.unibo.it>
2022-10-11 11:07:25 +0200

    logic error fixed: bad implementation

--- 2022/04/43-abstract-classes/src/main/java/it/unibo/inheritance/impl/AbstractBankAccount.java
+++ 2022/04/43-abstract-classes/src/main/java/it/unibo/inheritance/impl/AbstractBankAccount.java
@@ -6,64 +6,72 @@ import it.unibo.inheritance.api.BankAccount;
 public abstract class AbstractBankAccount implements BankAccount {
 
     protected static final double ATM_TRANSACTION_FEE = 1;
+    protected static final double MANAGEMENT_FEE = 5;
 
     private AccountHolder holder;
     private double balance;
     private int transactions;
 
+    /**
+     * Constructor.
+     * @param holder
+     * @param balance
+     */
     protected AbstractBankAccount(final AccountHolder holder, final double balance) { 
         this.holder = holder;
+        this.balance = balance;
+        this.transactions = 0;
     }
 
     @Override
-    public void chargeManagementFees(final int id) {
-        if (this.checkUser(id)) {
+    public final void chargeManagementFees(final int id) {
+        final double feeAmount = computeFee();
+        if (checkUser(id) && this.isWithDrawAllowed(feeAmount)) {
+            this.balance -= feeAmount;
             this.resetTransactions();
         }
     }
 
     @Override
-    public void deposit(final int id, final double amount) {
+    public final void deposit(final int id, final double amount) {
         this.transactionOp(id, amount);
     }
 
     @Override
-    public void depositFromATM(final int id, final double amount) {
+    public final void depositFromATM(final int id, final double amount) {
         this.deposit(id, amount - ATM_TRANSACTION_FEE);
     }
 
     @Override
-    public AccountHolder getAccountHolder() {
+    public final AccountHolder getAccountHolder() {
         return this.holder;
     }
 
     @Override
-    public double getBalance() {
+    public final double getBalance() {
         return this.balance;
     }
 
     @Override
-    public int getTransactionsCount() {
+    public final int getTransactionsCount() {
         return this.transactions;
     }
 
     @Override
-    public void withdraw(final int id, final double amount) {
-        this.transactionOp(id, -amount);
+    public final void withdraw(final int id, final double amount) {
+        if (this.isWithDrawAllowed(amount)) {
+            this.transactionOp(id, -amount);
+        }
     }
 
     @Override
-    public void withdrawFromATM(final int id, final double amount) {
+    public final void withdrawFromATM(final int id, final double amount) {
         this.withdraw(id, amount + ATM_TRANSACTION_FEE);
     }
 
     /*
      * Helper methods.
      */
-    public void setBalance(final double newBalance) {
-        this.balance = newBalance;
-    }
-
     private void resetTransactions() {
         this.transactions = 0;
     }
