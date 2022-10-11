13be7e3312c851906a0f1dc0378a8842dec3d040
AndrewGagliottiUnibo <andrew.gagliotti@studio.unibo.it>
2022-10-11 11:17:02 +0200

    formatting

--- 2022/04/44-45-rich-design/src/main/java/it/unibo/design/robot/test/TestRobotWithArms.java
+++ 2022/04/44-45-rich-design/src/main/java/it/unibo/design/robot/test/TestRobotWithArms.java
@@ -18,34 +18,41 @@ public final class TestRobotWithArms {
     }
 
     public static void main(final String[] args) {
+
         // Uncomment the method
-        
+
         final RobotWithArms walle = new RobotWithTwoArms("Wall-e");
         final String wallePosition = walle + "'s position";
         final String walleItems = walle + "'s items carried";
         final String walleConsumption = walle + "'s consumption is correct";
         assertEquals(wallePosition, new RobotPosition(0, 0), walle.getPosition());
         assertEquals(walleConsumption, BaseRobot.BATTERY_FULL,
-        walle.getBatteryLevel(), EPSILON);
+                walle.getBatteryLevel(), EPSILON);
         double consumptionEmpty = walle.getBatteryLevel();
+
         for (int i = 0; i < CYCLES; i++) {
-        walle.moveRight();
+            walle.moveRight();
         }
+
         consumptionEmpty -= walle.getBatteryLevel();
         walle.pickUp();
         assertEquals(walleItems, 1, walle.getCarriedItemsCount());
         double consumption1Item = walle.getBatteryLevel();
+
         for (int i = 0; i < CYCLES; i++) {
-        walle.moveUp();
+            walle.moveUp();
         }
+
         consumption1Item -= walle.getBatteryLevel();
         assertEquals(walleConsumption, true, consumption1Item > consumptionEmpty);
         walle.pickUp();
         assertEquals(walleItems, 2, walle.getCarriedItemsCount());
         double consumption2Item = walle.getBatteryLevel();
+
         for (int i = 0; i < CYCLES; i++) {
-        walle.moveUp();
+            walle.moveUp();
         }
+
         consumption2Item -= walle.getBatteryLevel();
         assertEquals(walleConsumption, true, consumption2Item > consumption1Item);
         walle.pickUp();
@@ -56,6 +63,5 @@ public final class TestRobotWithArms {
         assertEquals(walleItems, 0, walle.getCarriedItemsCount());
         walle.dropDown();
         assertEquals(walleItems, 0, walle.getCarriedItemsCount());
-        
     }
 }
