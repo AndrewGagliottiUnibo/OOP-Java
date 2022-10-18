948176cec1b45b3d19e067599c861a2ac74fba7b
AndrewGagliottiUnibo <andrew.gagliotti@studio.unibo.it>
2022-10-18 16:28:04 +0200

    test done

--- 2022/05/52-comparator/src/main/java/it/unibo/collections/comparators/UseSetWithOrder.java
+++ 2022/05/52-comparator/src/main/java/it/unibo/collections/comparators/UseSetWithOrder.java
@@ -3,10 +3,14 @@ package it.unibo.collections.comparators;
 import java.util.Set;
 import java.util.TreeSet;
 
+import it.unibo.collections.comparators.api.MyComparator;
+
 /**
  * 
  */
 public final class UseSetWithOrder {
+
+    private static final int LIMIT = 100;
 
     private UseSetWithOrder() {
     }
@@ -25,12 +29,17 @@ public final class UseSetWithOrder {
          * The comparator must convert the strings to double, then compare the doubles to find the biggest.
          * The comparator does not need to deal with the case of Strings which are not parseable as doubles.
          */
+        Set<String> set = new TreeSet<>(new MyComparator());
         /*
          * 2) Inserts in the set a hundred strings containing randomly generated numbers
          * (use Math.random(), and convert to String appropriately)
          */
+        for (int i = 0; i < LIMIT; i++) {
+            set.add(Double.toString(Math.random()));
+        }
         /*
          * 3) Prints the set, which must be ordered
          */
+        System.out.println(set);
     }
 }
