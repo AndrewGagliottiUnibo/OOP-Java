288832e87c9bfcc7361e3397514d5970afac1251
AndrewGagliottiUnibo <andrew.gagliotti@studio.unibo.it>
2022-10-18 16:17:34 +0200

    es 51 done

--- 2022/05/51-set/src/main/java/it/unibo/collections/sets/UseSet.java
+++ 2022/05/51-set/src/main/java/it/unibo/collections/sets/UseSet.java
@@ -1,6 +1,9 @@
 package it.unibo.collections.sets;
 
 import java.util.Collection;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.TreeSet;
 
 /**
  * Example class using {@link java.util.Set}.
@@ -24,22 +27,43 @@ public final class UseSet {
          *
          * 1) Builds a TreeSet containing Strings
          */
+        final Set<String> set = new TreeSet<>();
         /*
          * 2) Populates such Collection with all the Strings representing numbers ranging from "1" to
          * "20" (both included)
          */
+        for (int i = 0; i < ELEMS; i++) {
+            set.add(Integer.toString(i));
+        }
         /*
          * 3) Prints its content
          */
+        System.out.println(set);
         /*
          * 4) Removes all those strings whose represented number is divisible by three.
          * Note: the method removeIf(Predicate) is not allowed.
          */
+        final Iterator<String> iterator = set.iterator();
+        while (iterator.hasNext()) {
+            if (((Integer.parseInt(iterator.next())) % 3) == 0) {
+                iterator.remove();
+            }
+        }
         /*
          * 5) Prints the content of the Set using a for-each construct
          */
+        for (String elem : set) {
+            System.out.println(elem);
+        }
         /*
          * 6) Verifies whether all the numbers left in the set are even
          */
+        boolean marker = false;
+        for (String elem : set) {
+            if ((Integer.parseInt(elem) & 2) != 0) {
+                marker = true;
+            }
+        }
+        System.out.println("Are all elements even?: " + !marker);
     }
 }
