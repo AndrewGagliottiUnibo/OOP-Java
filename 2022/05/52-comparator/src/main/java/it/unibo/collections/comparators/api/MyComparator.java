9e6b86c35a206cdbac9e216163bb987315f01933
AndrewGagliottiUnibo <andrew.gagliotti@studio.unibo.it>
2022-10-18 16:27:57 +0200

    my comparator

---
+++ 2022/05/52-comparator/src/main/java/it/unibo/collections/comparators/api/MyComparator.java
@@ -0,0 +1,12 @@
+package it.unibo.collections.comparators.api;
+
+import java.util.Comparator;
+
+public class MyComparator implements Comparator<String> {
+
+    //I searched on the Javadoc for comparing double
+    @Override
+    public int compare(String o1, String o2) {
+        return Double.compare(Double.parseDouble(o1), Double.parseDouble(o2));
+    }   
+}
