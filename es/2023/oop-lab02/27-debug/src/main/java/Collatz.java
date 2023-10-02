9a5d04e54ecfedfecd78d0a3356e8e262b7c9102
Andrew Gagliotti <andrew.gagliotti@studio.unibo.it>
2023-10-02 11:59:14 +0200

    es8

--- es/2023/oop-lab02/27-debug/src/main/java/Collatz.java
+++ es/2023/oop-lab02/27-debug/src/main/java/Collatz.java
@@ -4,13 +4,18 @@ public class Collatz {
         var number = args.length > 0 ? Integer.parseInt(args[0]) : 1;
         System.out.println("Collatz sequence for " + number + ":");
         while (number != 1) {
-            System.out.print(number + " -> ");
-            if (number % 2 == 0) {
-                number /= 2;
+            if(number < 0 || number == 0) {
+                System.out.println("Overflow!");
+                System.exit(0);
             } else {
-                // Attempt to generate an overflow by multiplying the number by 3
-                // (this may result in overflow for large enough numbers)
-                number = number * 3 + 1;
+                System.out.print(number + " -> ");
+                if (number % 2 == 0) {
+                    number /= 2;
+                } else {
+                    // Attempt to generate an overflow by multiplying the number by 3
+                    // (this may result in overflow for large enough numbers)
+                    number = number * 3 + 1;
+                }
             }
         }
         System.out.println("1");
