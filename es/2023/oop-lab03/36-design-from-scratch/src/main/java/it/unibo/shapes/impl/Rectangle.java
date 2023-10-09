6ca7b3c00d78911637ae42bdbbc426e77c426e96
Andrew Gagliotti <andrew.gagliotti@studio.unibo.it>
2023-10-09 19:39:41 +0200

    new shapes

---
+++ es/2023/oop-lab03/36-design-from-scratch/src/main/java/it/unibo/shapes/impl/Rectangle.java
@@ -0,0 +1,31 @@
+package it.unibo.shapes.impl;
+
+import it.unibo.shapes.api.Polygon;
+
+public class Rectangle implements Polygon {
+
+    @Override
+    public double computeArea() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'computeArea'");
+    }
+
+    @Override
+    public double computePerimeter() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'computePerimeter'");
+    }
+
+    @Override
+    public String getDescription() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getDescription'");
+    }
+
+    @Override
+    public int getEdgeCount() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getEdgeCount'");
+    }
+    
+}
