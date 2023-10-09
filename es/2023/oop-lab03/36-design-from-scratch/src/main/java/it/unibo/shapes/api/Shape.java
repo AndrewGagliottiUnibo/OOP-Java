4455e4a7ffd8dcd0aadb1557f99ae96317bb8f2c
Andrew Gagliotti <andrew.gagliotti@studio.unibo.it>
2023-10-09 16:12:46 +0200

    preparing classes

---
+++ es/2023/oop-lab03/36-design-from-scratch/src/main/java/it/unibo/shapes/api/Shape.java
@@ -0,0 +1,10 @@
+package it.unibo.shapes.api;
+
+public interface Shape {
+    
+    double computeArea();
+
+    double computePerimeter();
+
+    String getDescription();
+}
---
+++ es/2023/oop-lab03/36-design-from-scratch/src/main/java/it/unibo/shapes/impl/Circle.java
@@ -0,0 +1,8 @@
+package it.unibo.shapes.impl;
+
+import it.unibo.shapes.api.Shape;
+import java.lang.Math;
+
+public class Circle implements Shape {
+    
+}
