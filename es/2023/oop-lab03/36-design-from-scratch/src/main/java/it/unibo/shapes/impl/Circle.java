0f72ed16f87de0c2fc66c50977cf42a6b165de3e
Andrew Gagliotti <andrew.gagliotti@studio.unibo.it>
2023-10-09 19:39:49 +0200

    circle implementation

--- es/2023/oop-lab03/36-design-from-scratch/src/main/java/it/unibo/shapes/impl/Circle.java
+++ es/2023/oop-lab03/36-design-from-scratch/src/main/java/it/unibo/shapes/impl/Circle.java
@@ -4,5 +4,45 @@ import it.unibo.shapes.api.Shape;
 import java.lang.Math;
 
 public class Circle implements Shape {
-    
+
+    private static final double PI = Math.PI;
+    private double radius;
+    // private double perimeter;
+    // private double area;
+
+    // Constructor
+    public Circle(final double radius) {
+        this.radius = radius;
+    }
+
+    // Shape interface methods
+	@Override
+	public double computeArea() {
+		return PI * Math.pow(radius, 2);
+	}
+
+	@Override
+	public double computePerimeter() {
+		return 2 * PI * radius;
+	}
+
+	@Override
+	public String getDescription() {
+		return this.toString();
+	}
+
+    // Getters and setters
+    public double getRadius() {
+        return this.radius;
+    }
+
+    public void setRadius(final double radius) {
+        this.radius = radius;
+    }
+
+    // toString()
+    @Override
+    public String toString() {
+        return "This is a circle with radius = " + radius + "cm";
+    }  
 }
