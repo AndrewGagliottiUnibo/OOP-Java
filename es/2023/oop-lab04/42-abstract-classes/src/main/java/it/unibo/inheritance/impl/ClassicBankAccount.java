be6d062e38e20f9d348dbe62a8655d76bb4c1e72
Andrew Gagliotti <andrew.gagliotti@studio.unibo.it>
2023-10-16 19:20:16 +0200

    beginning

---
+++ es/2023/oop-lab04/42-abstract-classes/src/main/java/it/unibo/inheritance/impl/ClassicBankAccount.java
@@ -0,0 +1,32 @@
+package it.unibo.inheritance.impl;
+
+import it.unibo.inheritance.api.AbstractBankAccount;
+import it.unibo.inheritance.api.AccountHolder;
+
+public class ClassicBankAccount extends AbstractBankAccount {
+
+    // Constructor
+    public ClassicBankAccount(AccountHolder accountHolder, double balance) {
+        super(accountHolder, balance);
+        //TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public void chargeManagementFees(int id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'chargeManagementFees'");
+    }
+
+    @Override
+    protected boolean isWithDrawAllowed(double value) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isWithDrawAllowed'");
+    }
+
+    @Override
+    protected double computeFee() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'computeFee'");
+    }
+    
+}
