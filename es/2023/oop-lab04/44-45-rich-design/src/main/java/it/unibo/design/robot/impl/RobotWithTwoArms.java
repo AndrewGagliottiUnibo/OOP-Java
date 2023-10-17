87336ad5c9695b3238ec72bbf3e93c58ce3210b2
Andrew Gagliotti <andrew.gagliotti@studio.unibo.it>
2023-10-17 16:14:16 +0200

    understanding behaviour

---
+++ es/2023/oop-lab04/44-45-rich-design/src/main/java/it/unibo/design/robot/impl/RobotWithTwoArms.java
@@ -0,0 +1,59 @@
+package it.unibo.design.robot.impl;
+
+import it.unibo.design.robot.api.RobotWithArms;
+
+public class RobotWithTwoArms extends BaseRobot implements RobotWithArms {
+
+    public static final double TRANSPORT_CONSUMPTION = 0.1;
+
+    private final BasicArm leftArm;
+    private final BasicArm rightArm;
+
+    // Constructor
+    public RobotWithTwoArms(final String robotName) {
+        super(robotName);
+        this.leftArm = new BasicArm("Left arm");
+        this.rightArm = new BasicArm("Right arm");
+    }
+
+    @Override
+    protected double getBatteryRequirementForMovement() {
+        return super.getBatteryRequirementForMovement() + this.getCarriedItemsCount() * TRANSPORT_CONSUMPTION;
+    }
+
+    // Actions performed
+    @Override
+    public boolean pickUp() {
+        if (this.rightArm.isGrabbed()) {
+            if (this.leftArm.isGrabbed()) {
+                return false;
+            }
+
+            this.pickItem(this.leftArm);
+        } else {
+            this.pickItem(this.rightArm);
+        }
+
+        return true;
+    }
+
+    @Override
+    public boolean dropDown() {
+        if (this.leftArm.isGrabbed()) {
+            this.leaveItem(this.leftArm);
+            return true;
+        }
+
+        if (this.rightArm.isGrabbed()) {
+            this.leaveItem(this.rightArm);
+            return true;
+        }
+
+        return false;
+    }
+
+    @Override
+    public int getCarriedItemsCount() {
+        return (this.leftArm.isGrabbed() ? 1 : 0) + (this.rightArm.isGrabbed() ? 1 : 0);
+    }
+}
