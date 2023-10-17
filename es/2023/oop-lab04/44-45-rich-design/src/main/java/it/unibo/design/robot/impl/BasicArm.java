51a04f87115ad4eb6b4871e5a8c5b946bb04bf88
Andrew Gagliotti <andrew.gagliotti@studio.unibo.it>
2023-10-17 16:01:38 +0200

    basic arm impl

--- es/2023/oop-lab04/44-45-rich-design/src/main/java/it/unibo/design/robot/impl/BasicArm.java
+++ es/2023/oop-lab04/44-45-rich-design/src/main/java/it/unibo/design/robot/impl/BasicArm.java
@@ -1,5 +1,42 @@
 package it.unibo.design.robot.impl;
 
 public class BasicArm {
-    
+
+    private static final double MOVING_CONSUMPTION = 0.2;
+    private static final double GRABBING_CONSUMPTION = 0.1;
+
+    private boolean isGrabbed;
+    private final String name;
+
+    // Constructor
+    public BasicArm(final String name) {
+        this.name = name;
+    }
+
+    // Actions performed
+    public void pickUp() {
+        this.isGrabbed = true;
+    }
+
+    public void dropDown() {
+        this.isGrabbed = false;
+    }
+
+    // Getters
+    public boolean isGrabbed() {
+        return this.isGrabbed;
+    }
+
+    public double getConsuptionForPickUp() {
+        return MOVING_CONSUMPTION + GRABBING_CONSUMPTION;
+    }
+
+    public double getConsuptionForDropDown() {
+        return MOVING_CONSUMPTION;
+    }
+
+    @Override
+    public String toString() {
+        return this.name;
+    }
 }
