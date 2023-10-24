6087b3152c427330ab40f53dbfdfc6303f589440
Andrew Gagliotti <andrew.gagliotti@studio.unibo.it>
2023-10-24 13:38:32 +0200

    es1

--- es/2023/oop-lab05/51-set/src/main/java/it/unibo/collections/sets/UseSet.java
+++ es/2023/oop-lab05/51-set/src/main/java/it/unibo/collections/sets/UseSet.java
@@ -1,6 +1,7 @@
 package it.unibo.collections.sets;
 
-import java.util.Collection;
+import java.util.Set;
+import java.util.TreeSet;
 
 /**
  * Example class using {@link java.util.Set}.
@@ -24,22 +25,54 @@ public final class UseSet {
          *
          * 1) Builds a TreeSet containing Strings
          */
+        Set<String> set = new TreeSet<>();
+
         /*
          * 2) Populates such Collection with all the Strings representing numbers ranging from "1" to
          * "20" (both included)
          */
+        for (int i = 1; i <= ELEMS; i++) {
+            set.add(Integer.toString(i));
+        }
+
         /*
          * 3) Prints its content
          */
+        System.out.println(set);
+
         /*
          * 4) Removes all those strings whose represented number is divisible by three.
          * Note: the method removeIf(Predicate) is not allowed.
          */
+        var it = set.iterator();
+
+        while(it.hasNext()) {
+            var elem = it.next();
+            if(Integer.parseInt(elem) % 3 == 0) {
+                it.remove();
+            }
+        }
+
         /*
          * 5) Prints the content of the Set using a for-each construct
          */
+        for(String elem : set) {
+            System.out.println(Integer.parseInt(elem));
+
+        }
+
         /*
          * 6) Verifies whether all the numbers left in the set are even
          */
+        var passCondition = true;
+        
+        for(String elem : set) {
+            if(Integer.parseInt(elem) % 2 != 0) {
+                System.out.println("Error in finding only even numbers: " + Integer.parseInt(elem));
+                passCondition = false;
+            }
+        }
+
+        System.out.println("All numbers are even? " + passCondition);
     }
 }
