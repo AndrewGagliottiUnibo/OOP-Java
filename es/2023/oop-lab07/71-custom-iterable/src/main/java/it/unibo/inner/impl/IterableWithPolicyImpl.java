b8ecbfba6f10b4fa056edc3246db758c160f4ed0
Andrew Gagliotti <andrew.gagliotti@studio.unibo.it>
2023-11-08 20:40:40 +0100

    added anonymous class

---
+++ es/2023/oop-lab07/71-custom-iterable/src/main/java/it/unibo/inner/impl/IterableWithPolicyImpl.java
@@ -0,0 +1,47 @@
+package it.unibo.inner.impl;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import it.unibo.inner.api.IterableWithPolicy;
+import it.unibo.inner.api.Predicate;
+
+public class IterableWithPolicyImpl<T> implements IterableWithPolicy<T> {
+
+    private final List<T> elements;
+
+    public IterableWithPolicyImpl(final T[] elements) {
+        this.elements = Arrays.asList(elements);
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        
+        return new Iterator<T>() {
+
+            private int current = 0;
+
+            @Override
+            public boolean hasNext() {
+                // THinking..
+                return false;
+            }
+
+            @Override
+            public T next() {
+                if(!hasNext()) {
+                    return elements.get(current++);
+                } else {
+                    throw new NoSuchElementException();
+                }
+            }
+        };
+    }
+
+    @Override
+    public void setIterationPolicy(final Predicate<T> filter) {
+        // empty method
+    }  
+}
